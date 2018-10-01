d70dab40624fe9367a9fd1465f6fa6e4a11e9707
Rocio Latorre <41635062+rociolatorreortiz@users.noreply.github.com>
2018-10-01 10:29:30 -0500

    Add files via upload

---
+++ 10inversion banco/10inversion banco/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10inversion_banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*10) Una persona desea invertir su dinero en un banco, el cual le otorga un 2% de interés. 
+             * Cual será la cantidad de dinero que esta persona tendrá al cabo de un año si la ganancia de cada mes es reinvertida?.*/
+
+            Console.WriteLine("Ingrese el valor del dinero que dejara en el banco");
+            string valor = Console.ReadLine();//pide numero
+            float valorParseado = float.Parse(valor);
+
+            float valor1 = valorParseado;
+            float g = 0;
+
+
+            for (int i = 1; i <= 12; i++)
+            {
+                g = valor1 + (valorParseado * 2/100);
+                valorParseado = g + valorParseado;
+
+                Console.WriteLine("La ganancia del dinero que esta persona tendra al cabo del mes {0}",i+" es: "+g);
+            }
+
+            float va = valor1 + g;
+            Console.WriteLine("La cantidad de dinero que esta persona tendra al cabo de un ano es: {0}",va);
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 11numero menor/11numero menor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11numero_menor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*11) Encontrar el menor valor de un conjunto de n números dados.*/
+
+            Console.WriteLine("Ingrese el numero limite");
+            string nl = Console.ReadLine();//pide numero
+            float nlParseado = float.Parse(nl);
+
+             Console.WriteLine("Ingrese el numero 1");
+            string n1 = Console.ReadLine();//pide numero
+            float n1Parseado = float.Parse(n1);
+            
+            for (int i = 2; i <= nlParseado; i++)
+            {
+                Console.WriteLine("Ingrese el numero {0}", i);
+                string n = Console.ReadLine();//pide numero
+                float nParseado = float.Parse(n);
+
+                if (nParseado < n1Parseado)
+                {
+                    n1Parseado = nParseado;
+                }
+            }
+            Console.WriteLine("El numero menor es: {0}", n1);
+                        
+            Console.ReadKey();
+        }
+    }
+}
